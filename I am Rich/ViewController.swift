cf2620fbd9e6faf0b81d8c81a31a56bab2b0a208
kuu7life <kuu7life@gmail.com>
2022-04-05 15:44:34 +0800

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Kutman Amangeldiev on 2022/4/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
